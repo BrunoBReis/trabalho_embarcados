ed260b8ccf454baee37453111a453069f7900dcc
Bruno Braganca dos Reis <brunobragancadosreis@gmail.com>
2026-07-15 03:28:31 -0300

    lint: enxuga comentario do crc16

--- common/pacote/crc16.c
+++ common/pacote/crc16.c
@@ -1,7 +1,6 @@
 #include "crc16.h"
 
-// Implementacao bit a bit (didatica; ha versoes com tabela, mais
-// rapidas, desnecessario para 18 bytes a cada 30 s). Para cada bit da
+// Implementacao bit a bit. Para cada bit da
 // mensagem: desloca o CRC e, se "vazou" um 1, aplica o polinomio (XOR).
 // E a divisao polinomial em GF(2) do apendice de qualquer datasheet.
 uint16_t crc16_ccitt(const uint8_t *dados, size_t tamanho) {
